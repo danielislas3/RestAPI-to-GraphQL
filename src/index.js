a56502f68e03a0bff9c1b7b917c3065517ae23c5
Daniel Orio <danielislas3@live.com>
2019-10-22 17:14:45 -0500

    prueba oneCharacter

--- src/index.js
+++ src/index.js
@@ -1,35 +1,36 @@
 const fetch = require("node-fetch");
-const { ApolloServer, gql } = require("apollo-server");
+const {
+  ApolloServer,
+  gql
+} = require("apollo-server");
 
-// This is a (sample) collection of books we'll be able to query
-// the GraphQL server for.  A more complete example might fetch
-// from an existing data source like a REST API or database.
-const books = [
-  {
-    title: "Harry Potter and the Chamber of Secrets",
-    author: "J.K. Rowling"
-  },
-  {
-    title: "Jurassic Park",
-    author: "Michael Crichton"
-  }
-];
+
 
 // Type definitions define the "shape" of your data and specify
 // which ways the data can be fetched from the GraphQL server.
-const typeDefs = gql`
+const typeDefs = gql `
   # Comments in GraphQL are defined with the hash (#) symbol.
 
-  # This "Book" type can be used in other type declarations.
-  type Book {
-    title: String
-    author: String
+  enum characterStatus {
+    Alive
+    Dead
+    unknown
+  }
+  type Character {
+    name: String
+    id: ID
+    status:characterStatus
+    episode:[String]
+    gender:String
+    url: String
+
   }
 
   # The "Query" type is the root of all GraphQL queries.
   # (A "Mutation" type will be covered later on.)
   type Query {
-    books: [Book]
+    characters:[Character]
+    character(id:ID!):Character
   }
 `;
 
@@ -37,54 +38,31 @@ const typeDefs = gql`
 // schema.  We'll retrieve books from the "books" array above.
 const resolvers = {
   Query: {
-    books: () => books
+    characters: () => fetchCharacters(),
+    character: (parent,args) => {
+      const {id} = args
+      return fetchCharacterById(id)
+    }
   }
 };
 
 // In the most basic sense, the ApolloServer can be started
 // by passing type definitions (typeDefs) and the resolvers
 // responsible for fetching the data for those types.
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
 
 // This `listen` method launches a web-server.  Existing apps
 // can utilize middleware options, which we'll discuss later.
-server.listen().then(({ url }) => {
+server.listen().then(({
+  url
+}) => {
   console.log(`🚀  Server ready at ${url}`);
 });
 
-// mock data for characters
-const characters = [
-  {
-    name: "Rick Sanchez",
-    id: 1,
-    status: "Alive",
-    episodes: [
-      "https://rickandmortyapi.com/api/episode/1",
-      "https://rickandmortyapi.com/api/episode/2"
-    ]
-  },
-  {
-    name: "Morty Smith",
-    id: 2,
-    status: "Alive",
-    episodes: [
-      "https://rickandmortyapi.com/api/episode/1",
-      "https://rickandmortyapi.com/api/episode/3"
-    ]
-  }
-];
 
-// mock data for episodes
-const episodes = [
-  {
-    name: "Pilot",
-    id: 1
-  },
-  {
-    name: "Lawnmower Dog",
-    id: 2
-  }
-];
 
 function fetchEpisodes() {
   // More info about the fetch function? https://github.com/bitinn/node-fetch#json
